127efbbdef9b1e2c4747fa94590c46a15a4bc0b9
Pranav Malik <86750720+Pranav2625@users.noreply.github.com>
2022-08-10 23:22:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from functools import partial
+
+
+root = Tk()
+root.title("Multiplayer Mode") # Window title
+
+double_frame = Frame(root, width=700, height=500, bg="green") # Window frame
+double_frame.grid()
+
+
+root.mainloop() # Loops the program until stopped/exited
