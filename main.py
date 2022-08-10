8f933f88e30abc529f3bca8dfe687a30b56f307b
Pranav Malik <86750720+Pranav2625@users.noreply.github.com>
2022-08-10 23:25:38 +0000

    Dealers side

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ root.title("Multiplayer Mode") # Window title
 double_frame = Frame(root, width=700, height=500, bg="green") # Window frame
 double_frame.grid()
 
+dealer_side = Label(double_frame, text="Dealer", font="Times 14 bold",  # Dealer's Side, 
+                    bg="#733f19", fg="white", padx=330, pady=12)  # of the table
+dealer_side.place(x=-10, y=0)
+
 
 root.mainloop() # Loops the program until stopped/exited
